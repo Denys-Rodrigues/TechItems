8fe99ff1521957e3cb90d7e3b40ce5917264fd1e
DW Rodrigues <denys.w.r@hotmail.com>
2022-06-15 20:03:10 -0300

    Criada a Model Produto e a AppCont

---
+++ Context/AppCont.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechItems.Models;
+
+namespace TechItems.Context
+{
+    public class AppCont : DbContext
+    {
+        public AppCont(DbContextOptions<AppCont> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechItems.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
