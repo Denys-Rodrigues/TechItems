86e88f6d5928c6c1c589a1a441339f52e044bd48
DW Rodrigues <denys.w.r@hotmail.com>
2022-06-15 20:10:25 -0300

    Adicionado o Construtor Serviço na Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<TechItems.Context.AppCont>(options =>
+{
+    options.UseSqlServer(builder
+        .Configuration
+        .GetConnectionString("DbTechItems"));
+});
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
