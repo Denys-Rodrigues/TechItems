4518fb11d145315ca07951b4406206876adee2e8
DW Rodrigues <denys.w.r@hotmail.com>
2022-06-15 21:10:33 -0300

    Adicionada a Action e View de Edit

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -59,6 +59,55 @@ namespace TechItems.Controllers
             return View(produto);
         }
 
+        // GET: Produtos/Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var produto = await _appCont.Produtos.FindAsync(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return View(produto);
+        }
+
+        // POST: Produtos/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description")] Produto produto)
+        {
+            if (id != produto.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _appCont.Update(produto);
+                    await _appCont.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProdutoExist(produto.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(produto);
+        }
+
         // GET: Produtos/Delete
         public async Task<IActionResult> Delete(long? id)
         {
@@ -87,6 +136,11 @@ namespace TechItems.Controllers
             _appCont.Produtos.Remove(produto);
             await _appCont.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProdutoExist(long id)
+        {
+            return _appCont.Produtos.Any(e => e.Id == id);
         }
     }
 }
